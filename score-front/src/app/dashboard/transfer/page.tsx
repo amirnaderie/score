4288d8f545ca8920fbf127538185733d143489a1
Amir Naderi <amirnaderie@gmail.com>
2025-08-23 16:30:37 +0330

    change error message in transfer page

--- score-front/src/app/dashboard/transfer/page.tsx
+++ score-front/src/app/dashboard/transfer/page.tsx
@@ -84,11 +84,11 @@ export default function TransferDashboard() {
     <div className="container mx-auto p-2">
       <SearchForm onSearch={handleSearch} loading={loading} />
 
-      {error && (
+      {/* {error && (
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
           {error.message}
         </div>
-      )}
+      )} */}
 
       <TransferTable
         transfers={transfers}
